63c65c1c12f66eb3185952183b65cf52b0ca91e5
Hamgule <hsj6831@naver.com>
2023-04-28 20:07:07 +0900

    Additional functions implemented

--- tico.c
+++ tico.c
@@ -2,37 +2,71 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
+#include <math.h>
 
-#define MAX_INT 127
-#define MIN_INT -128
-#define INST_LEN 15
+#define MAX_SINT 127
+#define MIN_SINT -128
+#define MAX_UINT 255
+#define MIN_UINT 0
 #define MEM_SIZ 256
+#define MEM_VIS 32
+
+/* ENUMS */
+typedef enum { 
+  NO_OPR, OPD_NUM, 
+  WF_COLON, WF_DQ,
+  NUM_IPT, NUM_ADR, NUM_OPD, NUM_VAL, NUM_ASN
+} CompileErrorType;
 
 typedef enum {
-  VALUE, READ, WRITE, ASSIGN, MOVE, LOAD,
-  STORE, ADD, MINUS, MULT, MOD,
-  EQ, LESS, JUMP, JUMPIF, TERM
+  OVFL, NOT_NUM
+} NumErrorType;
+
+typedef enum { VALUE,  
+  READ,   WRITE,  ASSIGN, MOVE, 
+  LOAD,   STORE,  ADD,    MINUS, 
+  MULT,   MOD,    EQ,     LESS, 
+  JUMP,   JUMPIF, TERM,   TYPE_LEN
 } Type;
 
-typedef union {
-  Type type;
-  int num;
-} Inst;
-
+/* STRUCTURE */
 typedef struct {
-  char *line;
-  Type type;
-  int regs[3];
-  int value;
+  unsigned char operands[3];
+  Type operator;
+} Instruction;
+
+/* UNION */
+typedef union {
+  char value;
+  Instruction inst;
 } MemoryCell;
 
-char *readLine();
-void initMem();
-void visMem();
-void splitAdrInst(char *str, int *addr, char **inst);
-void saveToMem(int addr, char *str); 
-Type toType(char *str);
-int execute(int addr);
+/* FUNCTION PROTOTYPES */
+// string-integer extended functions
+int intlen(int i);
+int extractNumber(char *str);
+int ctoi(char *str, int *num, int min, int max);
+
+// tico main functions
+char *readLine      ();
+void initMem        ();
+void visMem         ();
+void ignoreComment  (char *line);
+void splitAdrInst   (char *str,  int *addr, char **inst);
+void saveToMem      (int  addr,  char *str, char *line); 
+Type toType         (char *str);
+int  execute        (int  addr);
+int ticoInput       ();
+void ticoOutput     (int  n);
+
+// Error-handling related functions
+void fileIOError();
+void inputError(NumErrorType type);
+void compileError(CompileErrorType type, char *line);
+void numError(CompileErrorType cType, NumErrorType nType, char *line);
+void visError(char *msg, char *desc);
+void visCompileError(char *msg, char *desc, char *line);
 
 /* INSTRUCTION FUNCTIONS */
 int read   (int cur, int m);
@@ -54,26 +88,30 @@ int less   (int cur, int md, int mx, int my);
 int jump   (int cur, int m);
 int jumpIf (int cur, int m, int c);
 
+/* GLOBAL VARIABLES */
 FILE *fp;
-Inst insts[INST_LEN];
 MemoryCell memory[MEM_SIZ];
 int iptCount = 0;
 int optCount = 0;
+char *filename;
+Type curType;
+int r = 0, c = 1;
 
-const char *instructions[INST_LEN + 1] = {
-  "VALUE", "READ", "WRITE", "ASSIGN", "MOVE", "LOAD", 
+const char *instStrs[TYPE_LEN] = {
+  0x0, "READ", "WRITE", "ASSIGN", "MOVE", "LOAD", 
   "STORE", "ADD", "MINUS", "MULT", "MOD", 
   "EQ", "LESS", "JUMP", "JUMPIF", "TERM",
 };
 
-const  int params[INST_LEN + 1] = {
+const int params[TYPE_LEN] = {
   -1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 3, 1, 2, 0,
 };
 
 int main() {
-  char *filename = malloc(100);
+  filename = malloc(100);
   
   printf("\nProgram starts ...");
+  printf("\033[0;34m");
   printf("\n");
   printf("\n  *************************");
   printf("\n  *  TICO: TIny COmputer  *");
@@ -81,47 +119,91 @@ int main() {
   printf("\n");
   printf("\n  Enter the name of the executable file");
   printf("\n  > ");
+  printf("\033[0m");
   scanf("%[^\n]", filename);
   
-  printf("\n  '%s' file successfully loaded", filename);
-  printf("\n\n");
+  if (!(fp = fopen(filename, "r"))) fileIOError();
+  else {
+    initMem();
 
-  fp = fopen(filename, "r");
-  initMem();
+    char *s = 0x0;
+    while ((s = readLine())) {
+      char *l; c = 1;
+      int addr; char *line;
+      
+      r++;
+      ignoreComment(s);
+      if (!strcmp(s, "")) continue;
+      strcpy(l, s);
 
-  char *s = 0x0;
-  while ((s = readLine())) {
-    int address;
-    char *line;
-    
-    splitAdrInst(s, &address, &line);
-    if (strcmp(line, "")) memory[address].line = line;
-	}
+      splitAdrInst(s, &addr, &line);
+      if (strcmp(line, "")) saveToMem(addr, line, l);
+    }
 
-  for (int i = 0; i < MEM_SIZ; i++) {
-    char *str;
+    printf("\033[1;32m");
+    printf("\n  %s", filename);
+    printf("\033[0;32m");
+    printf(" file successfully loaded");
+    printf("\033[0m");
+    printf("\n\n");
 
-    str = malloc(strlen(memory[i].line) + 1);
-    strcpy(str, memory[i].line);
-    saveToMem(i, str);
-    
-    free(str);
+    visMem();
+
+    int cur = 0;
+    for (int i = 0; i < MEM_SIZ; i++) {
+      cur = execute(cur);
+      if (cur == -1) break;
+    }
   }
 
   visMem();
-
-  int cur = 0;
-  while (1) {
-    cur = execute(cur);
-    if (cur == -1) break;
-  }
-
-  printf("\n\nProgram terminated!\n");
+  printf("\n\nProgram terminated!\nGOOD BYE!\n\n");
 
   free(filename);
   
   return 0;
 }
+
+int intlen(int i) { return 1 + (i ? (int) log10(i) : 1); }
+
+int extractNumber(char *str) {
+  char numStr[10];
+  int count = 1, first;
+
+  for (int i = 0; i < strlen(str); i++) {
+    numStr[0] = '+';
+    if (!isdigit(str[i])) continue;
+    if (count == 1) first = i;
+    numStr[count++] = str[i];
+  }
+  numStr[count] = '\0';
+  if (str[first - 1] == '-') numStr[0] = '-';
+
+  return atoi(numStr);
+}
+
+// const to i
+// 0: no error
+// 1: " not exist
+// 2: " not closed
+// 3: non numeric
+// 4: overflow
+int ctoi(char *str, int *num, int min, int max) {
+  *num = extractNumber(str);
+
+  char *dqPtr = strchr(str, '\"');
+  if (!dqPtr || str[0] != '\"') return 1;
+
+  char *end = strchr(dqPtr + 1, '\"');
+  if (!end) return 2;
+  *end = '\0';
+
+  int val = atoi(str + 1);
+  if (!val && strcmp(str + 1, "0")) return 3;
+  if (val < min || val > max) return 4;
+  return 0;
+}
+
 
 char *readLine() {
   static char buf[BUFSIZ];
@@ -158,9 +240,68 @@ char *readLine() {
   return s;
 }
 
+void initMem() {
+  for (int addr = 0; addr < MEM_SIZ; addr++) {
+    memory[addr].value = 0;
+    memory[addr].inst.operator = VALUE;
+    for (int i = 0; i < 3; i++)
+      memory[addr].inst.operands[i] = 0;
+  }
+}
+
+void visMem() {
+  printf("\n┏");
+  for (int i = 0; i < 63; i++) printf("━"); printf("┓");
+  printf("\n┃ MEMORY (0~%d) %47s", MEM_VIS - 1, "");
+  if (MEM_VIS - 1 < 100) printf(" ");
+  if (MEM_VIS - 1 < 10) printf(" ");
+  printf("┃\n┠");
+  for (int i = 0; i < 31; i++) printf("─"); printf("┬");
+  for (int i = 0; i < 31; i++) printf("─"); printf("┨");
+  for (int addr = 0; addr < MEM_VIS / 2 + MEM_VIS % 2; addr++) {
+    printf("\n");
+    for (int i = 0; i < 2; i++) {
+      printf(i ? "│ " : "┃ ");
+      int idx = (MEM_VIS / 2 + MEM_VIS % 2) * i + addr;
+      if (idx == MEM_VIS) { printf("%30s", ""); break; }
+      printf("%3d | ", idx);
+      Instruction inst = memory[idx].inst;
+      if (inst.operator > 0 && inst.operator < TYPE_LEN) {
+        printf("%6s | ", instStrs[inst.operator]);
+        for (int j = 0; j < 3; j++) {
+          if (j >= params[inst.operator]) printf("     ");
+          else if (inst.operator == ASSIGN && j == 1) 
+            printf("%4d ", (char) inst.operands[j]);
+          else printf("%4d ", inst.operands[j]);
+        }
+        continue;
+      }
+      printf("%6s | %4d %9s ", "VALUE", memory[idx].value, "");
+    }
+    printf("┃");
+  }
+  printf("\n┗");
+  for (int i = 0; i < 31; i++) printf("━"); printf("┷");
+  for (int i = 0; i < 31; i++) printf("━"); printf("┛\n\n");
+}
+
+void ignoreComment(char *line) { 
+  char *ptr = strstr(line, "//"); 
+  if (ptr) *ptr = '\0'; 
+}
+
+/// @brief Splits a string containing an address and an instruction
+///        separated by a colon into its address and instruction components
+/// @param str  : the string to be split
+/// @param addr : a pointer to an integer that will hold the parsed address
+/// @param inst : a pointer to a char pointer that will point to the parsed instruction
 void splitAdrInst(char *str, int *addr, char **inst) {
+  char *line; 
   char *colonPtr, *addrPtr, *instPtr, *endPtr;
+  strcpy(line, str);
+
   colonPtr = strchr(str, ':');
+  if (!colonPtr) { c++; compileError(WF_COLON, line); }
   *colonPtr = '\0';
 
   addrPtr = str;
@@ -174,75 +315,148 @@ void splitAdrInst(char *str, int *addr, char **inst) {
   }
 
   *addr = atoi(addrPtr);
+
+  bool nonNumeric = !*addr && strcmp(addrPtr, "0");
+  bool overflow = *addr < MIN_UINT || *addr > MAX_UINT;
+  
+  if (overflow) numError(NUM_ADR, NOT_NUM, line);
+  if (nonNumeric) numError(NUM_ADR, OVFL, line);
+
   *inst = instPtr;
 }
 
-void saveToMem(int addr, char *str) {
-  if (strchr(str, ' ') != NULL || !strcmp(str, "TERM")) {
-    char *typeStr;
-    typeStr = strtok(str, " ");
+void saveToMem(int addr, char *str, char *line) {
+  bool hasSpace = strchr(str, ' ');
+
+  char *typeStr;
+  typeStr = strtok(str, " ");
+  curType = toType(typeStr);
+  c += 1 + intlen(addr);
+
+  // 존재하지 않는 Type 일 경우
+  bool noOperator = true;
+  noOperator &= strcmp(typeStr, "VALUE");
+  noOperator &= !curType;
+  noOperator &= !strcmp(typeStr, "0") || !atoi(typeStr);
+  
+  if (noOperator) { c++; compileError(NO_OPR, line); }
+
+  bool isTerm = !strcmp(str, "TERM");
+  bool isAssign = !strcmp(str, "ASSIGN");
+
+  // operator 뒤 공백이 있음
+  if (hasSpace) {
+    c += 1 + strlen(typeStr);
+    
+    // operator 가 TERM 임 -> ERROR
+    if (isTerm) compileError(OPD_NUM, line);
     char *token = strtok(NULL, " ");
 
-    for (int i = 0; i < 3; i++) {
-      if (!token) { 
-        memory[addr].regs[i] = params[memory[addr].type]; 
-        break;
+    int i = 0, operand = 0;
+    for (; i < 3; i++) {
+      if (!token) break;
+      
+      if (isAssign && i == 1) {
+        int err = ctoi(token, &operand, MIN_SINT, MAX_SINT);
+
+        switch (err) {
+          case 4: c += 2; numError(NUM_ASN, OVFL, line);
+          case 3: c += 2; numError(NUM_ASN, NOT_NUM, line);
+          case 2: c += 2 + intlen(operand);
+          case 1: compileError(WF_DQ, line);
+          default: break;
+        }
       }
-      memory[addr].regs[i] = atoi(token);
+      else {
+        c++;
+        operand = atoi(token);
+
+        bool nonNumeric = !operand && strcmp(token, "0");
+        bool overflow = operand < MIN_UINT || operand > MAX_UINT;
+
+        // 적절치 않은 operand
+        if (overflow) numError(NUM_OPD, OVFL, line);
+        if (nonNumeric) numError(NUM_OPD, NOT_NUM, line);
+
+        c += intlen(operand);
+      }
+
+      memory[addr].inst.operands[i] = operand;
+
       token = strtok(NULL, " ");
+
+      // operand 가 지정값보다 많음
+      if (token && i + 1 >= params[curType]) compileError(OPD_NUM, line);
+    }
+    
+    // operand 가 지정값보다 적음
+    if (i < params[curType]) compileError(OPD_NUM, line);
+
+    memory[addr].inst.operator = curType;
+  }
+  // operator 뒤 공백이 없음
+  else if (!isTerm) {
+    int n, err;
+    err = ctoi(str, &n, MIN_UINT, MAX_UINT);
+    
+    switch (err) {
+      case 4: numError(NUM_VAL, OVFL, line); break;
+      case 3: c += 2; numError(NUM_VAL, NOT_NUM, line); break;
+      case 2: c += 2 + intlen(n);
+      case 1: compileError(WF_DQ, line); break;
+      case 0: memory[addr].value = n; break;
+      default: break;
     }
 
-    memory[addr].type = toType(typeStr);
-  }
-  else {
-    char *dqPtr = strstr(str, "\"");
-    if (dqPtr) {
-      char *end = strstr(dqPtr + 1, "\"");
-      if (end) {
-        *end = '\0';
-        memory[addr].value = atoi(dqPtr + 1);
-      }
-    }
-  }
-}
+    // // if (str[0] != '\"') compileError(WF_DQ, line); 
+    // char *dqPtr = strstr(str, "\"");
+    
+    // // 쌍따옴표가 존재함
+    // if (dqPtr) {
+    //   char *end = strstr(dqPtr + 1, "\"");
+    //   if (end) {
+    //     *end = '\0';
+    //     int val = atoi(dqPtr + 1);
 
-void initMem() {
-  for (int i = 0; i < MEM_SIZ; i++) {
-    memory[i].line = "\"0\"";
-    for (int j = 0; j < 3; j++) memory[i].regs[j] = -1;
-  }
-}
+    //     bool nonNumeric = !val && strcmp(dqPtr + 1, "0");
+    //     bool overflow = val < MIN_SINT || val > MAX_SINT;
+    //     c += 2;
 
-void visMem() {
-  printf("\n***************\n MEMORY\n");
-  for (int i = 0; i < MEM_SIZ; i++) {
-    printf("\n %3d | ", i);
-    if (memory[i].type > 0 && memory[i].type < INST_LEN) {
-      printf("%8s | ", instructions[memory[i].type]);
-      for (int j = 0; j < params[memory[i].type]; j++) printf("%3d ", memory[i].regs[j]);
-      continue;
-    }
-    printf("%8s | %3d", "VALUE", memory[i].value);
+    //     // 적절치 않은 operand
+    //     if (overflow) numError(NUM_VAL, OVFL, line);
+    //     if (nonNumeric) numError(NUM_VAL, NOT_NUM, line);
+
+    //     memory[addr].value = val;
+    //   }
+    //   else {
+    //     c += 1 + strlen(str);
+    //     // compileError(WF_DQ, line);
+    //   }
+    // }
+
+    // 존재하는 Type 이 아님
+
+    // 쌍따옴표가 존재하지 않음
+    // else compileError(WF_DQ, line);
   }
-  printf("\n***************\n");
+  else if (!toType(str)) compileError(NO_OPR, line);
 }
 
 Type toType(char *str) {
-  for (int i = 0; i < INST_LEN; i++)
-    if (!strcmp(instructions[i], str)) return i;
-  return -1;
+  for (int i = 1; i < TYPE_LEN; i++)
+    if (!strcmp(instStrs[i], str)) return i;
+  return VALUE;
 }
 
 int execute(int addr) {
-  if (memory[addr].type < 0 || memory[addr].type > INST_LEN) return -2;
-  else if (!memory[addr].type) return addr + 1;
+  Instruction inst = memory[addr].inst;
+  if (!inst.operator) return addr + 1;
 
-  int reg0 = memory[addr].regs[0];
-  int reg1 = memory[addr].regs[1];
-  int reg2 = memory[addr].regs[2];
+  int reg0 = inst.operands[0];
+  int reg1 = inst.operands[1];
+  int reg2 = inst.operands[2];
 
-  // visMem();
-  switch (memory[addr].type) {
+  switch (memory[addr].inst.operator) {
     case READ   : return read  (addr, reg0);
     case WRITE  : return write (addr, reg0);
     case ASSIGN : return assign(addr, reg0, reg1);
@@ -264,17 +478,179 @@ int execute(int addr) {
   return -2;
 }
 
+int ticoInput() {
+  int n; 
+  bool overflow = false;
+  bool nonNumeric = false;
+
+  if (!iptCount++) printf("  << INPUT >>\n");
+  do {
+    if (overflow) inputError(OVFL);
+    if (nonNumeric) inputError(NOT_NUM);
+
+    printf("  %d > ", iptCount); 
+    nonNumeric |= !scanf("%d", &n);
+    while (getchar() != '\n');
+    overflow |= n < MIN_SINT || n > MAX_SINT;
+  } while (nonNumeric || overflow);
+
+  return n;
+}
+
+void ticoOutput(int n) {
+  if (!optCount) printf("\n  << OUTPUT >>\n  ");
+  printf("%d", n); optCount++;
+}
+
+
+
+void fileIOError() {
+  char *msg = malloc(100);
+  char *desc = malloc(100);
+
+  strcpy(msg, "File not found");
+  strcpy(desc, "\033[1;31m");
+  strcat(desc, filename);
+  strcat(desc, "\033[0;31m");
+  sprintf(desc, "%s file does not exist!", desc);
+
+  visError(msg, desc);
+
+  free(msg);
+  free(desc);
+}
+
+void inputError(NumErrorType type) {
+  char *msg = malloc(100);
+  char *desc = malloc(100);
+
+  switch (type) {
+  case OVFL: strcpy(desc, "Value overflow"); break;
+  case NOT_NUM: strcpy(desc, "Non-numeric Value"); break;
+  default: break;
+  }
+
+  strcpy(msg, "Input error");
+  strcat(desc, "\n\n\033[0;36m");
+  sprintf(desc, "%s Enter value %d ~ %d integer", desc, MIN_SINT, MAX_SINT);
+
+  visError(msg, desc);
+
+  free(msg);
+  free(desc);
+}
+
+void compileError(CompileErrorType type, char *line) {
+  char *msg = malloc(100);
+  char *desc = malloc(100);
+
+  switch (type) {
+    case NO_OPR: 
+      strcpy(msg, "Operator undefined"); 
+      strcpy(desc, "Existing operator expected"); 
+      break;
+    case OPD_NUM: 
+      strcpy(msg, "Unmatched number of operand");
+      sprintf(
+        desc, "%s operator must have %d operand%s", 
+        instStrs[curType], 
+        params[curType], 
+        params[curType] > 1 ? "s" : ""
+      );
+      break;
+    case WF_COLON: 
+      strcpy(msg, "Wrong input format"); 
+      strcpy(desc, ":"); 
+      break;
+    case WF_DQ: 
+      strcpy(msg, "Wrong input format"); 
+      strcpy(desc, "\""); 
+      break;
+    default: break;
+  }
+
+  visCompileError(msg, desc, line);
+
+  free(msg);
+  free(desc);
+  exit(-1);
+}
+
+void numError(CompileErrorType cType, NumErrorType nType, char *line) {
+  int min, max;
+  char *obj = malloc(100);
+  char *msg = malloc(100);
+  char *desc = malloc(100);
+  
+  switch (cType) {
+    case NUM_ASN: 
+      min = MIN_SINT; max = MAX_SINT; 
+      strcpy(obj, "Constant"); break;
+    case NUM_VAL: 
+      min = MIN_SINT; max = MAX_SINT; 
+      strcpy(obj, "Value"); break;
+    case NUM_ADR: 
+      min = MIN_UINT; max = MAX_UINT; 
+      strcpy(obj, "Address"); break;
+    case NUM_OPD: 
+      min = MIN_UINT; max = MAX_UINT; 
+      strcpy(obj, "Operand"); break;
+    default: break;
+  }
+
+  switch (nType) {
+    case OVFL:
+      sprintf(msg, "%s overflow", obj); 
+      sprintf(desc, "Set %s %d ~ %d integer", obj, min, max);
+      break;
+    case NOT_NUM: 
+      sprintf(msg, "Non-numeric %s", obj); 
+      sprintf(desc, "Set %s %d ~ %d integer", obj, min, max);
+      break;
+    default: break;
+  }
+
+  visCompileError(msg, desc, line);
+
+  free(obj);
+  free(msg);
+  free(desc);
+  exit(-1);
+}
+
+void visError(char *msg, char *desc) {
+  fprintf(stderr, "\033[0;31m");
+  fprintf(stderr, "\n  [ERROR] %s", msg);
+  fprintf(stderr, "\n  %s\n\n", desc);
+  fprintf(stderr, "\033[0m");
+}
+
+void visCompileError(char *msg, char *desc, char *line) {
+  fprintf(stderr, "\033[0;31m");
+  fprintf(stderr, "\n  [ERROR] Compile error");
+  fprintf(stderr, "\n  %s", msg);
+  fprintf(stderr, "\033[0m");
+
+  fprintf(stderr, "\033[1;37m");
+  fprintf(stderr, "\n\n  %s:%d:%d", filename, r, c);
+  fprintf(stderr, "\033[0m");
+  fprintf(stderr, "\n  %s\n  ", line);
+  
+  fprintf(stderr, "\033[0;32m");
+  for (int e = 0; e < c - 1; e++) fprintf(stderr, "~"); fprintf(stderr, "^\n");
+  for (int e = 0; e < c - 1; e++) fprintf(stderr, " ");
+  fprintf(stderr, "  %s\n\n", desc);
+  fprintf(stderr, "\033[0m");
+}
+
+
 int read(int cur, int m) {
-  int n;
-  if (!iptCount) printf("  << INPUT >>\n");
-  printf("  %d > ", ++iptCount); scanf("%d", &n);
+  int n = ticoInput();
   memory[m].value = n;
   return cur + 1;
 }
-
 int write(int cur, int m) { 
-  if (!optCount) printf("\n  << OUTPUT >>\n  ");
-  printf("%d", memory[m].value); optCount++;
+  ticoOutput(memory[m].value);
   return cur + 1; 
 }
 int assign(int cur, int m, int c) { 
